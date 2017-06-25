506ee12f8eb644bd37f2642237490c7496ee5a2c
Diogo <dioghx@gmail.com>
2017-06-25 13:04:54 -0300

    --Configurações profissional -Xml do searchble

---
+++ app/src/main/java/com/video/startup/editthevideodio/activity/ConfiguracoesActivity.java
@@ -0,0 +1,15 @@
+package com.video.startup.editthevideodio.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.video.startup.editthevideodio.R;
+
+public class ConfiguracoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes);
+    }
+}
